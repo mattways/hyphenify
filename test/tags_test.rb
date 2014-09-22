2cea16e5c25ef393040f9bac96e8b8704882c629
Mathías Montossi <mmontossi@museways.com>
2014-09-22 14:38:24 -0300

    Fix gemspec

--- test/tags_test.rb
+++ test/tags_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class TagsTest < ActionView::TestCase
 
-  test 'tags return id and class with hyphens' do
+  test 'tags hyphens' do
     tag(:div, class: 'the_class', id: 'test_id').tap do |tag|
       assert tag.include?('class="the-class" id="test-id"')
     end
   end
 
-  test 'forms tags return id and class with hyphens' do
+  test 'forms hyphens' do
     form_for(:test, url: 'http://test.com') do |f|
       f.text_field :id, class: 'the_class'
     end.tap do |form_tag|
