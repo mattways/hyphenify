b21feed307d87084c0256e200af4f50905230978
Mathías Montossi <mmontossi@buyin.io>
2016-12-13 14:55:58 -0300

    Minor updates

--- lib/dashify/railtie.rb
+++ lib/dashify/railtie.rb
@@ -1,10 +1,12 @@
 module Dashify
   class Railtie < Rails::Railtie
 
-    initializer 'dashify.extensions' do
-      ::ActionView::Helpers::TagHelper.prepend(
-        Dashify::Extensions::ActionView::TagHelper
-      )
+    initializer 'dashify.action_view' do
+      ActiveSupport.on_load :action_view do
+        ::ActionView::Helpers::TagHelper.prepend(
+          Dashify::Extensions::ActionView::TagHelper
+        )
+      end
     end
 
   end
